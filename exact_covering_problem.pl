a20a7aba0a315165e276b88422cacbb19fa90137
Konstantinos Gkentsidis <konsgken@gmail.com>
2021-08-08 16:58:34 +0300

    Update exact_covering_problem.pl

--- exact_covering_problem.pl
+++ exact_covering_problem.pl
@@ -1,61 +1,22 @@
-iscovered(_,[],[]).
-iscovered(I,[H|_],Result):-
-    member(I,H),Result=H.
-iscovered(I,[H|T],Result):-
-    \+(member(I,H)),iscovered(I,T,Result).
-iscovered(I,[H|T],Result):-
-    member(I,H),iscovered(I,T,Result),Result=[_|_].% Result is a non-empty list.
-%Result -> iscovered(d,[[c,e],[a,d,g],[b,c,f],[a,d,f],[b,g],[d,e,g]], Res)
+iscovered(I, [H|_T], H):-  member(I, H).
+iscovered(I, [_H|T], O):-  iscovered(I, T, O).
 
-%Residual is understood as the remaining elements here . Not sure though.
-residual(Is,I,Os,O,RIs,ROs):-remove(Is,I,RIs),remove(Os,O,ROs).
+remove(X, [X|T], T).
+remove(X, [H|T], [H|T1]):- remove(X, T, T1).
 
-%Sub Question-5
-findexactcovering(Is,Os,Result):-count2(Is,N),findall(Result_subsets,(subset(Os,Result_subsets)),Subsets),
-    findall(H,(member(H,Subsets),flatten(H,ResultH),count2(ResultH,N1),N=N1,same_elements(ResultH,Is)),Result).
+remove_list([], List, List).
+remove_list([H|T], List, Res):- remove(H, List, R1), remove_all(T, R1, Res).
 
-same_elements([], []).
-same_elements([H1|T1], L2) :- 
-   count2([H1|T1], X),
-   count2(L2, Y),
-   Y = X,
-   member(H1, L2),
-   myremove(H1, L2, Nl2),
-   same_elements(T1, Nl2).
+residual(I, Is, O, Os, RemainingI, RemainingO):- remove(I, Is, RemainingI), remove(O, Os, RemainingO).
 
-count2([],0).
-count2([_|T],N) :- count2(T,N1), N is N1 + 1.
-
-myremove(X, [X|T], T).   
-myremove(X, [H|T], [H|R]) :-
-   myremove(X, T, R).
-
-remove([X],X,[]).
-remove([X|T],X,T).
-remove([_|T],X,Result):-remove(T,X,Result).
-
-member(X,[X|_]).
-member(X,[_|T]):-member(X,T).
-
-%Subseting a list
+all_different([]).
+all_different([H|T]):- \+ member(H, T), all_different(T).
 
 subset([],[]).
-subset([First|Rest], [First|Sub]):-subset(Rest, Sub).
-subset([First|Rest], Sub):- subset(Rest, Sub).
+subset([First|Rest], [First|Sub]):- subset(Rest, Sub).
+subset([_First|Rest], Sub):- subset(Rest, Sub).
 
-%Flattening the list
+contains([], _).
+contains([H|T], List):- member(H, List), contains(T, List).
 
-flatten([], []).
-flatten(X, [X]) :- 
-	atomic(X), X  \== [].
-flatten([H|T], L3) :-
-	flatten(H, L1), flatten(T, L2), append(L1, L2, L3).
-       
-%Result
-%residual([a,b,c,d,e],a,[[a,b],[d,f,e,g]],[a,b],RIs,ROS)
-%[b, c, d, e]
-%[[d, f, e, g]]
-
-%findexactcovering([a,b,c,d],[[c,a],[b,d],[a,b],[b,c],[a,b,c]],Result)
-
-[[[c, a], [b, d]]]
+findexactcovering(Options, Res):- subset(Options, Res), flatten(Res, Flat), contains([a,b,c,d,e,f,g], Flat), all_different(Flat).
